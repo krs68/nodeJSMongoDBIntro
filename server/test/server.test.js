1ef21d9bd851446eb5514ca5179757c73dd39225
rakasarla <rakasarla@yahoo.com>
2019-01-23 04:50:41 -0500

    Added Get /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,11 +1,24 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
+
+const todos = [
+  { text: "First test toso" },
+  { text: "Second test toso" },
+  { text: "Third test toso" },
+  { text: "Fourth test toso" },
+  { text: "Fifth test toso" },
+  { text: "Sixth test toso" },
+  { text: "Seventh test toso" }
+];
+
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -14,7 +27,7 @@ describe('POST /todos', () => {
 
     request(app)
       .post('/todos')
-      .send({text})
+      .send({ text })
       .expect(200)
       .expect((res) => {
         expect(res.body.text).toBe(text);
@@ -24,7 +37,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({ text }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +56,23 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(7);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((resp) => {
+        // console.log('resp object is:', resp.body);
+        expect(resp.body.todo.length).toBe(7);
+      })
+      .end(done);
   });
 });
